9c6597f38940b4f2ff50eb56ad3cff6c73725edd
ZakirovInsaf <sadsquire@gmail.com>
2022-12-21 15:26:52 +0300

    init: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let c = "" + Math.abs(n);
+    let b = "";
+    for (let i = c.length - 1; i >= 0; i--) {
+        b += c[i];
+    }
+    return b;
+};
